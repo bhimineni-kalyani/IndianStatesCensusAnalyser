46600a0afb1085b5ee00e5a40344596b75c8a173
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-03-25 05:55:08 +1400

    UC1TC1.3

--- src/main/java/com/bridgelabz/CustomException.java
+++ src/main/java/com/bridgelabz/CustomException.java
@@ -1,9 +1,14 @@
 package com.bridgelabz;
 
 public class CustomException extends Exception {
+    public enum ExceptionType {
+        Wrongfile,
+        Wrongfiletype;
+    }
     public CustomException() {
     }
-    public CustomException(String s) {
+    public CustomException(String s,ExceptionType type) {
         super(s);
+        this.type = type;
     }
 }
--- src/main/java/com/bridgelabz/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -2,32 +2,43 @@ package com.bridgelabz;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class StateCensusAnalyser {
-        public int loadData(String path) throws CustomException {
-            int i=0;
-            try {
-                Reader reader= Files.newBufferedReader(Paths.get(path));
-                CsvToBean csvToBean = new CsvToBeanBuilder(reader).withType(CSVStateCensusAnalyser.class)
-                        .withIgnoreLeadingWhiteSpace(true)
-                        .build();
-                Iterator<CSVStateCensusAnalyser> csvStateCensusAnalyserIterator=csvToBean.iterator();
-                while(csvStateCensusAnalyserIterator.hasNext()) {
-                    CSVStateCensusAnalyser censusAnalyser=csvStateCensusAnalyserIterator.next();
-                    System.out.println("Name : " + censusAnalyser.getState());
-                    System.out.println("Email : " + censusAnalyser.getPopulation());
-                    System.out.println("PhoneNo : " + censusAnalyser.getAreaInSqKm());
-                    System.out.println("Country : " + censusAnalyser.getDensityPerSqKm());
-                    i++;
+    public int loadData(String path) throws CustomException {
+            if(path.contains(".csv")) {
+                int numofEnteries = 0;
+                try {
+                    Reader reader= Files.newBufferedReader(Paths.get(path));
+                    CsvToBean<CSVStateCensusAnalyser> csvToBean = new CsvToBeanBuilder(reader)
+                            .withType(CSVStateCensusAnalyser.class)
+                            .withIgnoreLeadingWhiteSpace(true)
+                            .build();
+
+                    Iterator<CSVStateCensusAnalyser> csvStateCensusAnalyserIterator=csvToBean.iterator();
+
+                        while(csvStateCensusAnalyserIterator.hasNext()) {
+                            CSVStateCensusAnalyser censusAnalyser=csvStateCensusAnalyserIterator.next();
+                            System.out.println("Name : " + censusAnalyser.getState());
+                            System.out.println("Email : " + censusAnalyser.getPopulation());
+                            System.out.println("PhoneNo : " + censusAnalyser.getAreaInSqKm());
+                            System.out.println("Country : " + censusAnalyser.getDensityPerSqKm());
+                            System.out.println("===");
+                            numofEnteries++;
+                        }
                 }
+                catch (IOException e) {
+                        throw new CustomException(e.getMessage(), CustomException.ExceptionType.Wrongfile);
+                }
+                return numofEnteries;
             }
-            catch (Exception e) {
-                e.printStackTrace();
+            else {
+                throw new CustomException("Wrongfiletype", CustomException.ExceptionType.Wrongfiletype);
             }
-            return i;
-        }
     }
+}
